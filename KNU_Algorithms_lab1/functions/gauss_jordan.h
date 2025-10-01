d490a9845ab4d602699c478afa3656540f6e3e13
typicalsoftwaredeveloper <minenkomykola510@gmail.com>
2025-10-01 21:57:30 +0300

    Files gauss_jordan.h and matrix.h were rewriten

--- KNU_Algorithms_lab1/functions/gauss_jordan.h
+++ KNU_Algorithms_lab1/functions/gauss_jordan.h
@@ -1,8 +1,36 @@
-#ifndef GAUSS_JORDAN_H
-#define GAUSS_JORDAN_H
+// Гаусс-Жордан для обернення матриці
+void gaussJordanMethod(MatrixData& data) {
 
-#include <matrix_data.h>
+    //перевірка матриці на квадратність
+    if (data.m != data.n) {
+        cout << "Matrix must be square." << endl;
+        return;
+    }
 
-void gaussJordanMethod(MatrixData& data);
+    //Створення та заповнення розширеної матриці (A | E)
+    MatrixData extendedMatrix(data.m, 2 * data.n);
+    extendedMatrix.fillExtendedMatrix();
 
-#endif
+    //Перевірка на невиродженість
+    double pivot = 0;
+    for (int i = 0; i < data.m; i++) {
+        pivot = extendedMatrix.matrix[i][i];
+
+        if (pivot == 0) {
+            cout << "Cannot invert matrix: zero pivot at row " << i << endl;
+            return;
+        }
+
+        //крок 1:обнулення елементів на головній діагоналі
+        extendedMatrix.multiplyRowByNumber(i, 1.0 / pivot);
+
+        //тут обнулення всіх елементів,які не на головній діагоналі
+        double factor = 0;
+        for (int j = 0; j < data.m; j++) {
+            if (j != i) {
+                factor = extendedMatrix.matrix[j][i];
+                extendedMatrix.substractRow(j, i, factor);
+            }
+        }
+    }
+}
--- KNU_Algorithms_lab1/matrix.h
+++ KNU_Algorithms_lab1/matrix.h
@@ -1,7 +1,4 @@
-#ifndef MATRIX_DATA_H
-#define MATRIX_DATA_H
-
-#include <iostream>
+﻿#include <iostream>
 #include <stdexcept>
 using namespace std;
 
@@ -21,4 +18,87 @@ struct MatrixData {
 
 };
 
-#endif
+MatrixData::MatrixData(int m, int n) {
+    if (m <= 1 || n <= 1) {
+        cerr << "Error: Matrix dimensions must be greater than 1." << endl;
+        exit(1);
+    }
+
+    this->m = m;
+    this->n = n;
+
+    matrix = new double* [m];
+    for (int i = 0; i < m; i++) {
+        matrix[i] = new double[n];
+    }
+}
+
+// Конструктор без параметрів + валідація
+MatrixData::MatrixData() {
+    do {
+        cout << "Matrix rows (must be > 1): ";
+        cin >> m;
+        cout << "Matrix columns (must be > 1): ";
+        cin >> n;
+
+        if (m <= 1 || n <= 1) {
+            cout << "Error: Both dimensions must be greater than 1. Try again.\n";
+        }
+    } while (m <= 1 || n <= 1);
+
+    matrix = new double* [m];
+    for (int i = 0; i < m; i++) {
+        matrix[i] = new double[n];
+        for (int j = 0; j < n; j++) {
+            cout << "Matrix[" << i << "][" << j << "]: ";
+            cin >> matrix[i][j];
+        }
+    }
+}
+
+// Звільнення памʼяті
+MatrixData::~MatrixData() {
+    for (int i = 0; i < m; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
+
+// Виведення матриці
+void MatrixData::show() const {
+    for (int i = 0; i < m; i++) {
+        cout << "| ";
+        for (int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << "|\n";
+    }
+}
+
+// Заповнення розширеної матриці (A | I)
+void MatrixData::fillExtendedMatrix() {
+    MatrixData extended(m, 2 * n);
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            extended.matrix[i][j] = matrix[i][j];
+        }
+        for (int j = n; j < extended.n; j++) {
+            extended.matrix[i][j] = (i == j - n) ? 1.0 : 0.0;
+        }
+    }
+}
+
+// Множення рядка на число
+void MatrixData::multiplyRowByNumber(int rowNumber, double number) {
+    for (int j = 0; j < n; j++) {
+        matrix[rowNumber][j] *= number;
+    }
+}
+
+// Віднімання (row1 - factor * row2)
+void MatrixData::substractRow(int row1, int row2, double factor) {
+    for (int j = 0; j < n; j++) {
+        matrix[row1][j] -= factor * matrix[row2][j];
+    }
+}
